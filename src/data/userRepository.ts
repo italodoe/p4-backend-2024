49640b854e5bf544130dfd1d5a1972d2095e4961
Italo Castillo <italogitdoe@gmail.com>
2024-05-25 21:41:47 +0200

    add find endpoints

--- src/data/userRepository.ts
+++ src/data/userRepository.ts
@@ -1,35 +1,58 @@
+import type { Prisma } from "@prisma/client";
 import { db } from "./db";
-
+export type UserOutput = Prisma.UserCreateInput;
 
 export const newUser = async (
-    email: string,
-    nick: string,
-    fullName: string | null,
-    admin: boolean = false,
-    videos = null,
-    comments = null
-  ) => {
-    return await db.user.create({
-      data: {
-        email,
-        nick,
-        fullName,
-        admin,
-      },
-    });
-  };
-
+  email: string,
+  nick: string,
+  fullName: string | null,
+  admin: boolean = false,
+  videos = null,
+  comments = null
+) => {
+  return await db.user.create({
+    data: {
+      email,
+      nick,
+      fullName,
+      admin,
+    },
+  });
+};
 
 /*
   Find
 */
 
 export const findAllUsers = async () => {
-    return await db.user.findMany({
-      include: {
-        videos: true,
-        comments: true,
-      },
-    });
-  };
-  
+  return await db.user.findMany({
+    include: {
+      videos: true,
+      comments: true,
+    },
+  });
+};
+
+export const findUserById = async (
+  userId: number
+): Promise<UserOutput | null> => {
+  return await db.user.findFirst({
+    where: { userId },
+  });
+};
+
+export const findUserByNick = async (
+  nick: string
+): Promise<UserOutput | null> => {
+  return await db.user.findFirst({
+    where: { nick },
+  });
+};
+
+export const findUserByEmail = async (
+  email: string
+): Promise<UserOutput | null> => {
+  return await db.user.findFirst({
+    where: { email },
+  });
+};
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,19 +3,67 @@ import { Router } from "express";
 import { z } from "zod";
 import { catchError } from "../errors";
 import { send } from "../response";
-import { findAllUsers } from "../data/userRepository";
-
+import {
+  findAllUsers,
+  findUserByEmail,
+  findUserById,
+  findUserByNick,
+} from "../data/userRepository";
 
 const router = Router();
+const idParamsSchema = z.object({
+  id: z.coerce.number(),
+});
+const nickParamsSchema = z.object({
+  nick: z.coerce.string().min(5).max(255),
+});
+const emailParamsSchema = z.object({
+  email: z.coerce.string().email().min(5),
+});
 
-router.get("/", 
-catchError(async (request, response) => {
+/*
+GET     /users/
+POST    /users/
+GET     /users/id/:id
+GET     /users/nick/:nick
+GET     /users/email/:email
+PUT     /users/id/:id
+DELETE  /users/id/:id
+*/
+
+router.get(
+  "/",
+  catchError(async (request, response) => {
     const users = await findAllUsers();
     send(response).ok(users);
   })
-)
+);
 
+router.get(
+  "/id/:id",
+  catchError(async (request, response, next) => {
+    const { id: userId } = idParamsSchema.parse(request.params);
+    const user = await findUserById(userId);
+    send(response).ok(user);
+  })
+);
 
+router.get(
+  "/nick/:nick",
+  catchError(async (request, response, next) => {
+    const { nick } = nickParamsSchema.parse(request.params);
+    const user = await findUserByNick(nick);
+    send(response).ok(user);
+  })
+);
 
+router.get(
+  "/email/:email",
+  catchError(async (request, response, next) => {
+    const { email } = emailParamsSchema.parse(request.params);
+    const user = await findUserByEmail(email);
+    send(response).ok(user);
+  })
+);
 
 export default router;
